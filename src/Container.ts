b73405d24752e97c17ff876c90700ac5efeeb2ab
Cristian Llanos <cristianllanos@outlook.com>
2020-08-22 13:30:29 -0500

    Use generics

--- src/Container.ts
+++ src/Container.ts
@@ -1,23 +1,36 @@
 export interface Container {
-    resolve(dependency: string): any
-    register(dependency: string, abstraction: () => any): Container
+    factory<T>(dependency: new () => T, abstraction: (container: Container) => T): Container
+    singleton<T>(dependency: new () => T, abstraction: (container: Container) => T): Container
+    resolve<T>(dependency: new () => T): T
 }
 
 export class Dependencies implements Container {
     definitions: Map<any, any>
+    singletons: Map<any, any>;
     constructor() {
-        this.definitions = new Map()
+        this.definitions = new Map();
+        this.singletons = new Map();
     }
-    register(dependency: string, abstraction: () => any): Container {
-        this.definitions.set(dependency, abstraction)
-        return this
+    singleton<T>(dependency: new () => T, abstraction: (container: Container) => T): Container {
+        console.log(dependency);
+        this.factory(dependency, (container: Container) => {
+            if (!this.singletons.has(dependency)) {
+                this.singletons.set(dependency, abstraction(container));
+            }
+            return this.singletons.get(dependency);
+        });
+        return this;
     }
-    resolve(dependency: string) {
+    factory<T>(dependency: new () => T, abstraction: (container: Container) => T): Container {
+        this.definitions.set(dependency, abstraction);
+        return this;
+    }
+    resolve<T>(dependency: new () => T ): T {
         if (!this.definitions.has(dependency)) {
-            throw new Error(`Unable to resolve dependency [${dependency}]`)
+            throw new Error(`Unable to resolve dependency [${dependency.name}]`);
         }
-        const abstraction = this.definitions.get(dependency)
-        return new abstraction()
+        const abstraction = this.definitions.get(dependency);
+        return abstraction(this);
     }
 }
 
--- tests/Container.test.ts
+++ tests/Container.test.ts
@@ -1,13 +1,55 @@
 import Container from '../src/index'
 
-test('resolves a dependency', () => {
-    const container = new Container()
-    const dependency = new SimpleDependency()
-    container.register(SimpleDependency.name, function () {
-        return dependency
-    })
-    const resolved = container.resolve(SimpleDependency.name)
+it('resolves a simple dependency', () => {
+    const dependency = new SimpleDependency();
+    const container = new Container();
+    container.singleton(SimpleDependency, (container: Container) => {
+        return dependency;
+    });
+    const resolved = container.resolve(SimpleDependency);
+    resolved.greet();
     expect(resolved).toBe(dependency);
 });
 
-class SimpleDependency {}
+it('cannot resolve a simple dependency', () => {
+    const container = new Container();
+    try {
+        container.resolve(AnotherSimpleDependency);
+    } catch (error) {
+        expect(error.message).toContain(AnotherSimpleDependency.name);
+    }
+});
+
+it('resolves a singleton dependency', () => {
+    const container = new Container();
+    container.singleton(SimpleDependency, () => {
+        return new SimpleDependency();
+    });
+    const firstResolved = container.resolve(SimpleDependency);
+    const secondResolved = container.resolve(SimpleDependency);
+    expect(firstResolved).toBe(secondResolved);
+});
+
+it('resolves a factory dependency', () => {
+    const container = new Container();
+    container.factory(SimpleDependency, () => {
+        return new SimpleDependency();
+    });
+    const firstResolved = container.resolve(SimpleDependency);
+    const secondResolved = container.resolve(SimpleDependency);
+    expect(firstResolved).toBeInstanceOf(SimpleDependency);
+    expect(secondResolved).toBeInstanceOf(SimpleDependency);
+    expect(firstResolved).not.toBe(secondResolved);
+});
+
+class SimpleDependency {
+    greet() {
+        return "I'm a simple dependency"
+    }
+}
+
+class AnotherSimpleDependency {
+    greet() {
+        return "I'm another simple dependency"
+    }
+}
